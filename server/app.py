1c5c0731f43c27f57ab2731caa576af4b9a48db8
enochblake <enochblake9@gmail.com>
2024-11-18 22:26:22 +0300

    working on deployment

--- server/app.py
+++ server/app.py
@@ -13,6 +13,7 @@ app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 
 # CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 CORS(app, supports_credentials=True, origins=["http://localhost:3000", "https://mazingira-application.onrender.com"])
+# postgresql://mazingira_82i9_user:Z5ytsHMJGey0mZGL75tbUVY08iEwVLsa@dpg-cstouv3qf0us73emhffg-a.oregon-postgres.render.com/mazingira_82i9
 
 app.config['SESSION_COOKIE_SAMESITE'] = 'None'  # or 'Strict' or 'Lax'
 app.config['SESSION_COOKIE_SECURE'] = True
